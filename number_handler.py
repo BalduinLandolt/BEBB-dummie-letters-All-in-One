7858d2e98c2e693e3482498cc26a82850fdd05ec
BalduinLandolt <balduin.landolt@unibas.ch>
2019-02-07 11:53:41 +0100

    Add: Reading already-in-use system numbers

--- number_handler.py
+++ number_handler.py
@@ -8,7 +8,19 @@ def get_used_sys_nos(force, test):
     print("Is Test Run: {}".format(test))
     is_test = test
 
+    all_numbers = get_all_sys_nos()
     """Do Stuff here"""
 
     print("Done getting Sys. Numbers.\n")
+    return
+
+
+def get_all_sys_nos():
+    print("loading numbers...")
+    res = []
+    with open("data/input/all_numbers.txt") as f:
+        res = f.readlines()
+    print("found numbers: " + str(len(res)))
+    print(res)
+    return res
 
