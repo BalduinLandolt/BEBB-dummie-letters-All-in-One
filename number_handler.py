24cc42f437d4dc8f9a702624aa720287a3c9e616
BalduinLandolt <balduin.landolt@unibas.ch>
2019-02-07 15:39:10 +0100

    Add: implemented general logic of getting the system numbers. Doesn't do anything yet.

--- number_handler.py
+++ number_handler.py
@@ -1,3 +1,5 @@
+import os
+
 force_all = False
 is_test = False
 
@@ -9,6 +11,7 @@ def get_used_sys_nos(force, test):
     is_test = test
 
     all_numbers = get_all_sys_nos()
+    used_numbers = get_used_sys_nos()
     """Do Stuff here"""
 
     print("Done getting Sys. Numbers.\n")
@@ -16,7 +19,7 @@ def get_used_sys_nos(force, test):
 
 
 def get_all_sys_nos():
-    print("loading numbers...")
+    print("loading 'all numbers'...")
     res = []
     with open("data/input/all_numbers.txt") as f:
         res = f.readlines()
@@ -24,3 +27,58 @@ def get_all_sys_nos():
     print(res)
     return res
 
+
+def get_used_sys_nos():
+    print("loading 'used numbers'...")
+
+    used_nos = []
+
+    if force_all:
+        used_nos = grab_used_nos()
+        write_used_nos(used_nos)
+    else:
+        if os.path.isfile("./data/tmp/existing_numbers.txt"):
+            print("File 'existing_numbers.txt' already exists. Loading list from cache instead.")
+            used_nos = read_used_nos()
+        else:
+            used_nos = grab_used_nos()
+            write_used_nos(used_nos)
+
+    print("Got Already Used System Numbers now.")
+    print(used_nos)
+
+    return used_nos
+
+
+def grab_used_nos():
+    """
+    print("getting a list of the contents of the directory...")
+    files = listdir("input/xml")
+    print("found:")
+    print(files)
+    print()
+    res = ""
+    for file in files:
+        print(file)
+        sys_no = get_by_name(len(file))
+        res += sys_no + "\n"
+
+    print("Result: ")
+    print(res)
+    with open("input/existing_numbers.txt", "w") as out_file:
+        print(res, file=out_file)
+
+    print("handled all files in folder.")
+    """
+
+
+def read_used_nos():
+    """
+
+    """
+
+
+def write_used_nos():
+    """
+
+    """
