83bc10375339b51698312de64574db282d53593a
BalduinLandolt <balduin.landolt@unibas.ch>
2019-02-09 11:27:46 +0100

    Add: started general logic of dummy writer

--- dummie_writer.py
+++ dummie_writer.py
@@ -1,15 +1,27 @@
 force_all = False
 is_test = False
+marc_data_list = []
 
-def write_dummies(force, test):
+def write_dummies(data_list, force, test):
     print("Writing Dummie XML Files to Output...")
     print("Is Force Run: {}".format(force))
+    global force_all
     force_all = force
     print("Is Test Run: {}".format(test))
+    global is_test
     is_test = test
+    print("Sets of Data: {}".format(len(data_list)))
+    global marc_data_list
+    marc_data_list = data_list
 
-    """Do Stuff here"""
+    if is_test:
+        write_dummy(marc_data_list[0])
+    else:
+        for d in marc_data_list:
+            write_dummy(d)
 
     print("Done writing Dummies.\n")
 
 
+def write_dummy(data_set):
+    """"""
--- main.py
+++ main.py
@@ -42,6 +42,6 @@ number_list = number_handler.get_sys_nos_to_work_with(force_all, is_test)
 
 marc_data = marc_handler.get_info_from_aleph(number_list, force_all, is_test)
 
-dummie_writer.write_dummies(force_all, is_test) # TODO add marc data
+dummie_writer.write_dummies(marc_data, force_all, is_test)
 
 print("Done.")
