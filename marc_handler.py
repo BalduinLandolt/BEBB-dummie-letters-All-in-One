99961cafd247feddafea70e25cdf00972e5e5332
BalduinLandolt <balduin.landolt@unibas.ch>
2019-03-22 16:29:19 +0100

    Add: Documentation

--- marc_handler.py
+++ marc_handler.py
@@ -21,10 +21,10 @@ def get_info_from_aleph(nos, force, test):
     """
     Umbrella method to get a list of data from the aleph catalogue.
 
-    :param nos:
-    :param force:
-    :param test:
-    :return:
+    :param nos: a list of all numbers that are supposed to be loaded
+    :param force: if true, do a force run
+    :param test: if true, do a test run
+    :return: a list of marc records for the system numbers from the input `nos`
     """
 
     print("Getting Meta Information from Aleph Catalogue...")
@@ -50,6 +50,14 @@ def get_info_from_aleph(nos, force, test):
 
 
 def get_marc(no):
+    """
+    gets marc record for a single entry
+
+    Convenience method that checks, if the marc record needs to be loaded (over read_mc()) or loaded from cache.
+    :param no: system number
+    :return: marc record for the system number.
+    """
+
     print("getting marc for: {}".format(no))
 
     if force_all:
@@ -57,13 +65,13 @@ def get_marc(no):
         return mc
 
     if os.path.isfile("data/tmp/marc/" + no + ".marc"):
-        return read_mc_from_cache(no)
+        return __read_mc_from_cache(no)
 
     mc = read_mc(no)
     return mc
 
 
-def read_mc_from_cache(no):
+def __read_mc_from_cache(no):
     with open("data/tmp/marc/" + no + ".marc", "rb") as f:
         data = f.read()
     reader = MARCReader(bytes(data), force_utf8=True, to_unicode=True)
@@ -72,79 +80,37 @@ def read_mc_from_cache(no):
     return tmp
 
 
-def write_to_cache(marc, no):
+def __write_to_cache(marc, no):
     if force_all or is_test or not os.path.isfile("data/tmp/marc/" + no + ".marc"):
         with open("data/tmp/marc/" + no + ".marc", "wb") as f:
             f.write(marc)
 
 
 def test_mc():
+    """
+    Method for testing purpose. Loads a single record instead of the entire list.
+
+    :return: A random marc entry to one system number from the list.
+    """
+
     import random
     no = no_list[random.randint(0, len(no_list)-1)]
     print("Testing: {}".format(no))
     mc = read_mc(no)
     print("Marc Data:")
     print(mc)
-    """
-    print("Test Data:")
-    print("date: {}".format(get_date(mc).encode('utf-8')))
-    print("author: {}".format(get_author(mc).encode('utf-8')))
-    print("recipient: {}".format(get_recipient(mc).encode('utf-8')))
-    print("work reference: {}".format(get_bernoulli_work_reference(mc).encode('utf-8')))
-    print("content info: {}".format(get_content_info(mc).encode('utf-8')))
-    print("acc. material: {}".format(get_accompanying_material(mc).encode('utf-8')))
-    print("bibl. info: {}".format(get_bibliographical_info(mc).encode('utf-8')))
-    print("creation form: {}".format(get_creation_form(mc).encode('utf-8')))
-    print("creation place: {}".format(get_creation_place(mc).encode('utf-8')))
-    print("description: {}".format(get_description(mc).encode('utf-8')))
-    print("e-manuscripta link: {}".format(get_emanuscript_link(mc).encode('utf-8')))
-    print("footnote: {}".format(get_footnote(mc).encode('utf-8')))
-    print("language: {}".format(get_language(mc).encode('utf-8')))
-    print("mentioned persons: {}".format(get_mentioned_persons(mc).encode('utf-8')))
-    print("phys. description: {}".format(get_physical_description(mc).encode('utf-8')))
-    print("reproduction info: {}".format(get_reproduction_info(mc).encode('utf-8')))
-    """
-    """
-    print("Test Data:")
-    print("date: {}".format(get_date(mc)))
-    print("author: {}".format(get_author(mc)))
-    print("recipient: {}".format(get_recipient(mc)))
-    print("work reference: {}".format(get_bernoulli_work_reference(mc)))
-    print("content info: {}".format(get_content_info(mc)))
-    print("acc. material: {}".format(get_accompanying_material(mc)))
-    print("bibl. info: {}".format(get_bibliographical_info(mc)))
-    print("creation form: {}".format(get_creation_form(mc)))
-    print("creation place: {}".format(get_creation_place(mc)))
-    print("description: {}".format(get_description(mc)))
-    print("e-manuscripta link: {}".format(get_emanuscript_link(mc)))
-    print("footnote: {}".format(get_footnote(mc)))
-    print("language: {}".format(get_language(mc)))
-    print("mentioned persons: {}".format(get_mentioned_persons(mc)))
-    print("phys. description: {}".format(get_physical_description(mc)))
-    print("reproduction info: {}".format(get_reproduction_info(mc)))
-    """
-    """
-    print("Test Data:")
-    print("date: {}".format(unicode(get_date(mc))))
-    print("author: {}".format(unicode(get_author(mc))))
-    print("recipient: {}".format(unicode(get_recipient(mc))))
-    print("work reference: {}".format(unicode(get_bernoulli_work_reference(mc))))
-    print("content info: {}".format(unicode(get_content_info(mc))))
-    print("acc. material: {}".format(unicode(get_accompanying_material(mc))))
-    print("bibl. info: {}".format(unicode(get_bibliographical_info(mc))))
-    print("creation form: {}".format(unicode(get_creation_form(mc))))
-    print("creation place: {}".format(unicode(get_creation_place(mc))))
-    print("description: {}".format(unicode(get_description(mc))))
-    print("e-manuscripta link: {}".format(unicode(get_emanuscript_link(mc))))
-    print("footnote: {}".format(unicode(get_footnote(mc))))
-    print("language: {}".format(unicode(get_language(mc))))
-    print("mentioned persons: {}".format(unicode(get_mentioned_persons(mc))))
-    print("phys. description: {}".format(unicode(get_physical_description(mc))))
-    print("reproduction info: {}".format(unicode(get_reproduction_info(mc))))"""
+
     return mc
 
 
 def read_mc(sys_no):
+    """
+    Loads marc data from aleph.unibas.ch for one single system number.
+
+    :param sys_no: System number to which the marc entry is to be loaded.
+    :return: marc binary for said system number.
+    """
+
     print("reading: "+sys_no)
 
     try:
@@ -159,7 +125,7 @@ def read_mc(sys_no):
         print("\n!!! Error: could not connect to aleph !!!\n")
         return
 
-    write_to_cache(data, sys_no)
+    __write_to_cache(data, sys_no)
 
     reader = MARCReader(bytes(data), force_utf8=True, to_unicode=True)
     tmp = next(reader)
@@ -168,17 +134,23 @@ def read_mc(sys_no):
 
 
 def get_system_number(record):
+    """
+    Reads the system number from marc data
+
+    :param record: binary marc record
+    :return: system number as string
+    """
+
     field = record.get_fields('035')
     no = field[0]['a'].encode('utf-8')
 
     return no
 
 
-""" 
-All the following functions are copied from Tobias' program for getting stuff from the wiki
-https://git.iml.unibas.ch/salsah-suite/api_import_scripts/blob/master/BEOL/BEBB/Z3950_aleph.py
-( also: https://git.iml.unibas.ch/salsah-suite/api_import_scripts/blob/master/BEOL/BEBB/BEBBParser.py )
-"""
+# All the following functions are copied from Tobias' program for getting stuff from the wiki
+# https://git.iml.unibas.ch/salsah-suite/api_import_scripts/blob/master/BEOL/BEBB/Z3950_aleph.py
+# ( also: https://git.iml.unibas.ch/salsah-suite/api_import_scripts/blob/master/BEOL/BEBB/BEBBParser.py )
+
 
 def get_date(records):
     date = None
@@ -186,7 +158,6 @@ def get_date(records):
         date = field['c'].encode('utf-8')
 
     return date
-
 
 
 def __check_for_gnd(marcField, GNDIndex):
@@ -213,6 +184,7 @@ def __check_for_gnd(marcField, GNDIndex):
         "role": role.encode('utf-8')
     }
 
+
 def get_author(records):
     author = []
     for field in records.get_fields('100'):
@@ -227,6 +199,7 @@ def get_author(records):
 
     return author
 
+
 def get_recipient(records):
     recipient = []
     for field in records.get_fields('700'):
@@ -237,12 +210,14 @@ def get_recipient(records):
 
     return recipient
 
+
 def get_mentioned_persons(records):
     mentioned = []
     for field in records.get_fields('600'):
         mentioned.append(__check_for_gnd(field, '0'))
 
     return mentioned
+
 
 def get_description(records):
     description = None
@@ -264,6 +239,7 @@ def get_description(records):
 
     return description
 
+
 def get_creation_form(records):
     creation_information = None
     for field in records.get_fields('250'):
@@ -271,6 +247,7 @@ def get_creation_form(records):
             creation_information = field['a'].encode('utf-8')
 
     return creation_information
+
 
 def get_creation_place(records):
     creation_place = None
@@ -289,8 +266,8 @@ def get_creation_place(records):
                     'gnd': field['0'].encode('utf-8')
                 }
 
-
     return creation_place
+
 
 def get_physical_description(records):
     physical_description = None
@@ -312,6 +289,7 @@ def get_physical_description(records):
 
     return physical_description
 
+
 def get_footnote(records):
     footnote = None
     for field in records.get_fields('500'):
@@ -319,6 +297,7 @@ def get_footnote(records):
             footnote = field['a'].encode('utf-8')
 
     return footnote
+
 
 def get_bibliographical_info(records):
     bibliographical_info = None
@@ -340,6 +319,7 @@ def get_bibliographical_info(records):
 
     return bibliographical_info
 
+
 def get_content_info(records):
     content_info = None
     for field in records.get_fields('520'):
@@ -348,6 +328,7 @@ def get_content_info(records):
 
     return content_info
 
+
 def get_accompanying_material(records):
     accompanying_material = None
     for field in records.get_fields('525'):
@@ -355,6 +336,7 @@ def get_accompanying_material(records):
             accompanying_material = field['a']
 
     return accompanying_material
+
 
 def get_reproduction_info(records):
     reproduction_info = None
@@ -406,6 +388,7 @@ def get_reproduction_info(records):
 
     return reproduction_info
 
+
 def get_language(records):
     language = None
     for field in records.get_fields('546'):
@@ -414,6 +397,7 @@ def get_language(records):
 
     return language
 
+
 def get_bernoulli_work_reference(records):
     bernoulli_work_reference = None
     for field in records.get_fields('596'):
@@ -421,6 +405,7 @@ def get_bernoulli_work_reference(records):
             bernoulli_work_reference = field['a']
 
     return bernoulli_work_reference
+
 
 def get_emanuscript_link(records):
     emanuscript_link = None
